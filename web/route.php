9836b0462e8ac023ed699837136e0b10058f3232
Ahmad Fauzi <fauzi.ahmad@gmail.com>
2020-05-31 12:02:42 +0700

    sdf

--- web/route.php
+++ web/route.php
@@ -9,12 +9,28 @@ return static function (SlimApp $app) use ($settings) {
         $group->group('/v0', function (RouteCollectorProxy $group) {
             $group->post('/users/login', Albatiqy\Slimlibs\Actions\Api\Login0Post::class); // <== jgn di jwt
             $group->post('/users/token', Albatiqy\Slimlibs\Actions\Api\Token0Post::class); // <== jgn di jwt
-            $group->group('/sys', function (RouteCollectorProxy $group) {
-                $group->group('/configs', function (RouteCollectorProxy $group) {
-                    $group->put('', Albatiqy\Slimlibs\Actions\Api\Config\V0Put::class);
-                    $group->group('/{key}', function (RouteCollectorProxy $group) {
-                        $group->get('', Albatiqy\Slimlibs\Actions\Api\Config\V0Get::class);
-                        $group->delete('', Albatiqy\Slimlibs\Actions\Api\Config\V0Delete::class);
+            $group->group('', function (RouteCollectorProxy $group) {
+                $group->group('/sys', function (RouteCollectorProxy $group) {
+                    $group->group('/configs', function (RouteCollectorProxy $group) {
+                        $group->put('', Albatiqy\Slimlibs\Actions\Api\Config\V0Put::class);
+                        $group->group('/{key}', function (RouteCollectorProxy $group) {
+                            $group->get('', Albatiqy\Slimlibs\Actions\Api\Config\V0Get::class);
+                            $group->delete('', Albatiqy\Slimlibs\Actions\Api\Config\V0Delete::class);
+                        });
+                    });
+                });
+                $group->group('/module', function (RouteCollectorProxy $group) {
+                    $group->group('/media', function (RouteCollectorProxy $group) {
+                        $group->get('/browse', App\Actions\Api\Module\Media\Browse0Get::class);
+                        $group->get('/recentfiles', App\Actions\Api\Module\Media\RecentFiles0Get::class);
+                    });
+                });
+                $group->group('/users', function (RouteCollectorProxy $group) {
+                    $group->get('', App\Actions\Api\User\V0Get::class);
+                    $group->group('/{id:U\d{9}}', function (RouteCollectorProxy $group) {
+                        $group->get('', App\Actions\Api\User\V0Get::class);
+                        $group->put('', App\Actions\Api\User\V0Put::class);
+                        $group->delete('', App\Actions\Api\User\V0Delete::class);
                     });
                 });
             })->add(Jwt::class);
@@ -24,5 +40,11 @@ return static function (SlimApp $app) use ($settings) {
     $app->get('/js/modules/globals.js', Albatiqy\Slimlibs\Actions\Web\GlobalsJsGet::class); // create api global json
     $app->get($settings['login_path'], App\Actions\Web\LoginGet::class);
     $app->get('/mlogin', App\Actions\Web\Modules\LoginGet::class);
+    $app->group('/resources', function (RouteCollectorProxy $group) {
+        $group->get('/imgcache/{id}', App\Actions\Resource\ImageCacheGet::class);
+        $group->group('/media', function (RouteCollectorProxy $group) {
+            $group->get('/{path:.*}', App\Actions\Resource\MediaGet::class);
+        });
+    });
     require APP_DIR.'/routes/main.php';
 };
