54728f69a2a6f488884a454b52b9f80b2102d4fe
Ahmad Fauzi <fauzi.ahmad@gmail.com>
2020-06-02 05:36:37 +0700

    sf

--- requires/container.php
+++ requires/container.php
@@ -2,16 +2,16 @@
 
 return static function ($settings) {
     $providers = require LIBS_DIR . '/requires/providers.php';
-    
+
     $providers[Albatiqy\Slimlibs\Providers\Auth\AuthInterface::class] = $settings['auth_provider'];
 
     $container = Albatiqy\Slimlibs\Container\Container::getInstance($providers);
     $container->set('settings', $settings);
     $container->mapAlias(Psr\Log\LoggerInterface::class, 'monolog');
-    $container->setFunction('getBaseUrl', function($container){
+    $container->setFunction('getBaseUrl', function($container, $https=false){
         $request = $container->get('request');
         $uri = $request->getUri();
-        return $uri->getScheme() . '://' . $uri->getHost().\BASE_PATH;
+        return  ($https?'https':$uri->getScheme()) . '://' . $uri->getHost().\BASE_PATH;
     });
     /*
     $container->defineExtends(Albatiqy\Slimlibs\Providers\Database\DbService::class, function($subclass){
