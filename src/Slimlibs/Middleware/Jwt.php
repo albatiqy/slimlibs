3e25c79ac3e72592160be4403c937d5d6ef3a71a
Ahmad Fauzi <fauzi.ahmad@gmail.com>
2020-06-11 17:24:43 +0700

    dsdfsd

--- src/Slimlibs/Middleware/Jwt.php
+++ src/Slimlibs/Middleware/Jwt.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Albatiqy\Slimlibs\Result\Exception\UnauthorizedException;
 use Albatiqy\Slimlibs\Result\Exception\NoAccessException;
 use Albatiqy\Slimlibs\Providers\Auth\AuthInterface;
 
