397eb92fe30f88d8a006c80a2d213258a7945379
Ahmad Fauzi <fauzi.ahmad@gmail.com>
2020-06-18 17:48:47 +0700

    sdfdsf

--- src/Slimlibs/Middleware/GoogleCrawler.php
+++ src/Slimlibs/Middleware/GoogleCrawler.php
@@ -6,19 +6,34 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Albatiqy\Slimlibs\Support\Helper\Env;
+use Albatiqy\Slimlibs\Container\Container;
+use Slim\App;
 
 final class GoogleCrawler implements MiddlewareInterface {
 
+    protected const CACHE_EXPIRES = 18000;
+
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
-        /*
+        $profile = 'google';
         $useragent = $request->getHeaderLine('User-Agent');
         if (\strpos($useragent, 'Google') !== false) {
             if ($this->validateGoogleBotIP(Env::getClientIp())) {
-                $request = $request->withAttribute('google-crawler', 'google');
+                $container = Container::getInstance();
+                $settings = $container->get(App::class);
+                $pathuri = \substr($request->getUri()->getPath(), \strlen(\BASE_PATH));
+                $cacheFile = $settings['cache']['base_dir'] . '/'. $profile.'-pages' . ($pathuri != '/' ? $pathuri : '/index') . '.php';
+                if (\file_exists($cacheFile)) {
+                    if (\time()-self::CACHE_EXPIRES < \filemtime($cacheFile)) {
+                        $app = $container->get('app');
+                        $responseFactory = $app->getResponseFactory();
+                        $response = $responseFactory->createResponse(200);
+                        $response->getBody()->write(\file_get_contents($cacheFile));
+                        return $response;
+                    }
+                }
+                $request = $request->withAttribute('google-crawler', $profile);
             }
         }
-        */
-        $request = $request->withAttribute('google-crawler', 'google');
         return $handler->handle($request);
     }
 
