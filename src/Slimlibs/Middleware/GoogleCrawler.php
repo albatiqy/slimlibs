f8600140b218a7ee9e78d4ff6ebd8ed2303fbc37
Ahmad Fauzi <fauzi.ahmad@gmail.com>
2020-06-18 14:52:37 +0700

    dsf

---
+++ src/Slimlibs/Middleware/GoogleCrawler.php
@@ -0,0 +1,30 @@
+<?php declare (strict_types = 1);
+namespace Albatiqy\Slimlibs\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Albatiqy\Slimlibs\Support\Helper\Env;
+
+final class GoogleCrawler implements MiddlewareInterface {
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
+        /*
+        $useragent = $request->getHeaderLine('User-Agent');
+        if (\strpos($useragent, 'Google') !== false) {
+            if ($this->validateGoogleBotIP(Env::getClientIp())) {
+                $request = $request->withAttribute('google-crawler', true);
+            }
+        }
+        */
+        $request = $request->withAttribute('google-crawler', true);
+        return $handler->handle($request);
+    }
+
+    private function validateGoogleBotIP($ip) {
+        $hostname = \gethostbyaddr($ip); //"crawl-66-249-66-1.googlebot.com"
+        return \preg_match('/\.googlebot\.com$/i', $hostname);
+    }
+
+}
