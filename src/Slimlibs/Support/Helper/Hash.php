934b25658733bd0ceabe597da166e85f3e8210f8
Ahmad Fauzi <fauzi.ahmad@gmail.com>
2020-07-30 20:35:46 +0700

    dfds

--- src/Slimlibs/Support/Helper/Hash.php
+++ src/Slimlibs/Support/Helper/Hash.php
@@ -12,4 +12,42 @@ final class Hash {
             \mt_rand(0, 0xffff), \mt_rand(0, 0xffff), \mt_rand(0, 0xffff)
         );
     }
+
+    public static function genCouponCode($j=4) {
+        $alpha = \str_split('ABCDEFGHJKLMNPQRSTUVWXYZ');
+        $alpha_cnt = \count($alpha)-1;
+        $digit = \str_split('23456789');
+        $digit_cnt = \count($digit)-1;
+        $string = '';
+        for ($i=0;$i<$j;$i++) {
+            \srand(\intval((double)\microtime()*1234567));
+            $x = \mt_rand(0, 1);
+            switch ($x) {
+                case 0:
+                    $string .= $alpha[\mt_rand(0,$alpha_cnt)];
+                break;
+                case 1:
+                    $string .= $digit[\mt_rand(0,$digit_cnt)];
+                break;
+            }
+        }
+        return $string;
+    }
+
+    public static function genTimeRandomString($j=8) {
+        $string = '';
+        for ($i=0;$i<$j;$i++) {
+            \srand(\intval((double)\microtime()*1234567));
+            $x = \mt_rand(0, 1);
+            switch ($x) {
+                case 0:
+                    $string .= \chr(\mt_rand(65,90));
+                break;
+                case 1:
+                    $string .= \chr(\mt_rand(48,57));
+                break;
+            }
+        }
+        return $string;
+    }
 }
--- src/Slimlibs/Support/Helper/Str.php
+++ src/Slimlibs/Support/Helper/Str.php
@@ -40,4 +40,12 @@ final class Str {
 
         return $ret;
     }
+
+    public static function urlSafeBase64_encode($string) {
+        return \rtrim(\strtr(\base64_encode($string), '+/', '-_'), '=');
+    }
+
+    public static function urlSafeBase64_decode($string) {
+        return \base64_decode(\str_pad(\strtr($string, '-_', '+/'), \strlen($string) % 4, '=', \STR_PAD_RIGHT));
+    }
 }
